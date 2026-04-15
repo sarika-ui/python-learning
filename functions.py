48b179ac26df4c93c8eed0ba5f5d22b5a512adcb
sarika-ui <kumarisarika1144@gmail.com>
2026-04-15 23:17:02 +0530

    funtions

---
+++ functions.py
@@ -0,0 +1,12 @@
+# there are two types of funtions one is buildin funtions like print(), len() , and the othe is user define funtion and that we can create using the def funtion with the name 
+# def greet():
+#     print("hello funtion")
+    
+# greet()
+# function parameter
+
+def greet(name):
+    print(f"hello {name}")
+
+greet("sarika")
+
